275f3885024bb5a865ca2b854cf6ffe5a899deca
aidan daffa <muhammad.aidan@ui.ac.id>
2021-09-11 10:00:45 +0700

    CS1_2nd

---
+++ jmart/aidanJmartBO/Jmart.java
@@ -0,0 +1,49 @@
+package aidanJmartBO;
+
+
+/**
+ * Write a description of class Jmart here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+public class Jmart
+{
+    public static void main (String args[])
+    {
+        
+    }
+    
+    public int getPromo(){
+        return;
+    }
+    
+    public String getCustomer(){
+        return;
+    }
+        
+    public float getDiscountPercentage(int a, int b){
+        return;
+    }
+    
+    public int getDiscountPrice(int a, float b){
+        return;
+    }
+    
+    public int getOriginalPrice(int a, float b){
+        return;
+    }
+    
+    public float getCommissionMultiplier(){
+        return;
+    }
+    
+    public int getAdjustedPrice(int a){
+        return;
+    }
+    
+    public int getAdminFee(int a){
+        return;
+    }
+}
